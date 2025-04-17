a3757fe6c2c716f3d0301291c1648b3bed67c28d
kiran <kiranuthaman85@gmail.com>
2025-04-17 16:53:55 +0530

    feature updated

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -7,7 +7,7 @@ function Features() {
     <div className="w-full flex flex-col justify-center items-center gap-8 px-6 sm:px-12 mt-5">
       <div className="flex flex-col md:flex-row gap-8 w-full justify-evenly items-center sm:pb-5">
         <Relife />
-        <Zwr />
+        <Zwr/>
       </div>
     </div>
   )
